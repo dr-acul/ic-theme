eeca183eefd51dc7914b9dc6f190c28ba3faa7a7
dr-acul <dr.acul@posteo.de>
2016-04-17 20:09:00 +0200

    - added single post page - added post headers - fixed some layout issues

--- front-page.php
+++ front-page.php
@@ -8,10 +8,12 @@
  * @since increare 0.01
  * 
  */
+ ?>
  
-get_header(); ?>
+ <?php get_header(); ?>
 
-<section>
+<div id="primary" class="content-area">
+	<main role="main">
 	
 <?php
 	if ( is_front_page() ) {
@@ -22,26 +24,26 @@ get_header(); ?>
 				get_post( get_theme_mod( 'portfolio_selection_1') ),
 				get_post( get_theme_mod( 'portfolio_selection_2') ),
 		);
-		?>
+/* TODO: implement multi-row portfolio -> 0.04 */ 
+?>
 		<div class="ic_portfolio_row clearfix">
 		<?php
 		global $post;
 		foreach ( $portfolio_posts as $post) : setup_postdata($post); ?>
-			<div class="ic_portfolio_col">
-				<h1><?php the_title(); ?></h1>
+			<div id="<?php the_ID(); ?>" <?php post_class(); ?>>
+				<header>
+					<h1 class="post-title"><?php the_title(); ?></h1>
+				</header>
 				<?php the_content(); ?>
 			</div><!-- .ic_portfolio_col -->
 		<?php endforeach; ?>
-		reset_post_data();
 		</div><!-- .ic_portfolio_row -->
 		<?php
-	} else {
-		while (have_posts() ) : the_post();
-			the_content();
-		endwhile;
 	}
 ?>
 </section>
+	</main>
+</div><!-- .content-area -->
 <?php
 /*
 	wp_link_pages();
---
+++ single.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * IN|creare single post layout
+ * 
+ * @package increare
+ * @subpackage increare-template
+ * @since increare 0.01
+ * 
+ */
+ 
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main role="main">
+		<?php while ( have_posts() ) : the_post(); ?>
+		<article>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header>
+				<?php the_title( '<h1 class="post-title">', '</h1>'	); ?>
+			</header>
+				<?php the_content(); ?>
+			</div><!-- #post-## -->
+		</article>
+<?php
+endwhile;
+?>
+	</main><!-- .site-main -->
+</div><!-- .content-area -->
+<?php
+/*
+	wp_link_pages();
+
+	get_sidebar();
+*/
+get_footer();
